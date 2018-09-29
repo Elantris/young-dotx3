fb4b16ff80fb95fb9332a65f4f4d66fdd4e271d6
Elantris <j88107333@gmail.com>
2018-09-29 18:05:38 +0800

    Add player

--- src/App.js
+++ src/App.js
@@ -1,21 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import playlist from './playlist'
+import YouTubePlayer from './YouTubePlayer'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      year: '2013',
+      videoIndex: 0
+    }
+  }
+
+  handleSelectYear (year) {
+    return event => {
+      this.setState({
+        year,
+        videoIndex: 0
+      })
+    }
+  }
+
+  handleSelectVideo (videoIndex) {
+    return event => {
+      this.setState({
+        videoIndex
+      })
+    }
+  }
+
+  playNext () {
+    return () => {
+      this.setState({
+        videoIndex: (this.state.videoIndex + 1) % playlist[this.state.year].length
+      })
+    }
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App container'>
+
+        <h1>青春點點點 循環播放器</h1>
+        <p>YouTube <a href='https://www.youtube.com/channel/UCxxUtGu7ABDdFAgifw-Zk6A'>啤酒肥花</a> 剪輯</p>
+
+        <div className='columns mb-16'>
+          <div className='column col-5'>
+            <div className='btn-group btn-group-block'>
+              {Object.keys(playlist).map(v => (
+                <div
+                  key={`year-select-${v}`}
+                  className={`btn ${v === this.state.year ? 'active' : ''}`}
+                  onClick={this.handleSelectYear(v)}
+                >
+                  {v}
+                </div>
+              ))}
+            </div></div>
+          <div className='column col-7' />
+        </div>
+
+        <div className='divider mb-16' />
+
+        <div className='columns mb-16'>
+          <div className='column col-5'>
+            <ul className='menu playlist'>
+              <li className='divider' data-content='LINKS' />
+              {playlist[this.state.year].map((v, i) => (
+                <li key={`video-link-${v.id}`} className='menu-item'>
+                  <a
+                    className={`c-hand ${i === this.state.videoIndex ? 'active' : ''}`}
+                    onClick={this.handleSelectVideo(i)}
+                  >
+                    {v.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='column col-7'>
+            <YouTubePlayer
+              videoId={playlist[this.state.year][this.state.videoIndex].id}
+              playNext={this.playNext()}
+            />
+          </div>
+        </div>
+
+        <div className='divider mb-16' />
+
+        <footer className='text-center'>
+          <a href='github.com/Elantris'><i class='fab fa-github' /> Elantris</a>
+        </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/YouTubePlayer.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+class YouTubePlayer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      player: {}
+    }
+  }
+
+  componentDidMount () {
+    let tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    let firstScriptTag = document.getElementsByTagName('script')[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+    let player
+    window.onYouTubeIframeAPIReady = () => {
+      player = new window.YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: this.props.videoId,
+        playerVars: {
+          rel: '0'
+        },
+        events: {
+          'onStateChange': (event) => {
+            if (event.data === window.YT.PlayerState.ENDED) {
+              this.props.playNext()
+            }
+          }
+        }
+      })
+      this.setState({
+        player
+      })
+    }
+  }
+
+  componentDidUpdate (prevProps) {
+    if (this.props.videoId !== prevProps.videoId) {
+      this.state.player.loadVideoById(this.props.videoId)
+    }
+  }
+
+  render () {
+    return (
+      <div className='YouTubePlayer text-right'>
+        <div id='player' />
+      </div>
+    )
+  }
+}
+
+export default YouTubePlayer
